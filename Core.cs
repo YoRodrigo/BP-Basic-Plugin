2a1fe2dc03fa45d1e0c31628a8a4880152eb5801
YoRodrigo <85424553+YoRodrigo@users.noreply.github.com>
2022-07-17 21:45:21 -0500

    Add files via upload

---
+++ Core.cs
@@ -0,0 +1,22 @@
+﻿using BrokeProtocol.API;
+using BrokeProtocol.Managers;
+
+namespace BPLatam
+{
+    public class Core : Plugin
+    {
+        public static Core Instance { get; set; } // Instancia (Acceder a las variables dentro del Core)
+        public string url = "https://www.haziel.tk/BPBasics"; // Variable url
+        public SvManager SvManager { get; set; } // SvManager
+
+        public Core()
+        {
+            Instance = this; // Seteamos instancia como la instancia donde se ejecuta el plugin.
+            Info = new PluginInfo("BPLatam", "bpl") // Informacion del plugin
+            {
+                Description = "Plugin madre de Latam Server",
+                Website = "https://www.haziel.tk"
+            };
+        }
+    }
+}
